44d0bb505545eea4fc9bd4022f0cf1e5b902dba6
Ruben <rubdeivis@gmail.com>
2026-01-04 18:12:13 -0400

    feat: add youtube-shorts-blocker script

---
+++ youtube-shorts-blocker.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Youtube shorts blocker
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Delete shorts sections
+// @author       rubenguc
+// @match        *://*.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?bb=1&domain=youtube.com
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const removeShorts = () => {
+    const sections = [
+      "grid-shelf-view-model",
+      "ytd-rich-section-renderer",
+      "ytd-reel-shelf-renderer",
+      "ytd-guide-entry-renderer:has(a[title='Shorts'])",
+      "ytd-mini-guide-entry-renderer[aria-label='Shorts']",
+    ];
+
+    sections.forEach((selector) => {
+      document.querySelectorAll(selector).forEach((el) => {
+        if (el.innerText.toLowerCase().includes("shorts")) {
+          el.remove();
+        }
+      });
+    });
+
+    const shortLinks = document.querySelectorAll("a[href^='/shorts/']");
+
+    shortLinks.forEach((link) => {
+      const videoContainer = link.closest(
+        "ytd-video-renderer, ytd-rich-item-renderer, ytd-grid-video-renderer",
+      );
+      if (videoContainer) {
+        videoContainer.remove();
+      }
+    });
+  };
+
+  const observer = new MutationObserver(() => {
+    removeShorts();
+  });
+
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
+
+  window.addEventListener("load", removeShorts);
+  window.addEventListener("yt-navigate-finish", removeShorts);
+})();
